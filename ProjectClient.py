5194b5780a02f96541d3201b884faad2172325b4
arad1410 <63020716+arad1410@users.noreply.github.com>
2021-02-02 23:08:48 +0200

    first version

--- ProjectClient.py
+++ ProjectClient.py
@@ -33,46 +33,6 @@ class Client(object):
             # server
             self.rcv_file()
 
-    def rcv_file(self):
-        self.answer = self.answer.split("$")
-        progress = tqdm.tqdm(range(int(self.answer[0])), f"Receiving {self.answer[1]}", unit="B", unit_scale=True,
-                             unit_divisor=1024)
-        with open(self.answer[1], "wb") as f:
-            data = self.my_socket.recv(BUFFER_SIZE)
-            while "done sending".encode() not in data:
-                bytes_read = self.aes.decrypt_aes(data, self.key)
-                f.write(bytes_read)
-                data = self.my_socket.recv(BUFFER_SIZE)
-            print(data)
-            data = data[0:-12]
-            print(data)
-            bytes_read = self.aes.decrypt_aes(data, self.key)
-            f.write(bytes_read)
-            print("done")
-
-    def send_file(self, file):
-        progress = tqdm.tqdm(range(os.path.getsize(file)), f"Sending {file}", unit="B", unit_scale=True,
-                             unit_divisor=1024)
-        with open(file, "rb") as f:
-            f = f.read()
-            f = self.aes.encrypt_aes(f, self.key)
-            f += "done sending".encode()
-            counter = 0
-            for _ in progress:
-                # read the bytes from the file
-                bytes_read = f[BUFFER_SIZE * counter:BUFFER_SIZE * (counter + 1)]
-                print(bytes_read)
-                if not bytes_read:
-                    # file transmitting is done
-                    print("done")
-                    break
-                # we use sendall to assure transimission in
-                # busy networks
-                counter += 1
-                self.my_socket.sendall(bytes_read)
-                # update the progress bar
-                progress.update(len(bytes_read))
-
     def send_message(self, msg):
         print(self.aes.encrypt_aes(pickle.dumps(msg), self.key))
         self.my_socket.send(self.aes.encrypt_aes(pickle.dumps(msg), self.key))

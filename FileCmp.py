d88c9beed08e3077010dc1eb50af0f34ad1b5a5c
arad1410 <63020716+arad1410@users.noreply.github.com>
2021-03-07 15:53:46 +0200

    first version

--- FileCmp.py
+++ FileCmp.py
@@ -7,11 +7,15 @@ class FileCmp(object):
             "r").readlines()
         self.diff_other_file = {}
         self.diff_my_file = {}
+        self.diff_my_lines = []
+        self.diff_other_lines = []
         self.chang_rate = 2 / 3
-        self.line = 0
+        self.other_index = 0
+        self.my_index = 0
         self.deleted_lines = []
+        self.other_deleted_lines = []
 
-    def cmp_line(self, my_line, other_line, line):
+    def cmp_line(self, my_line, other_line):
         counter = 0
         answer = []
         diff_counter = 0
@@ -22,38 +26,74 @@ class FileCmp(object):
                 diff_counter += 1
             counter += 1
         if self.chang_rate * len(my_line) >= diff_counter:
-            self.diff_other_file[line] = answer
-            self.diff_my_file[line] = answer
+            self.diff_other_file[self.other_index] = answer
+            self.diff_my_file[self.my_index] = answer
             return True
         else:
-            self.diff_other_file[line] = -1
-            self.diff_my_file[line] = -1
+            self.diff_other_file[self.other_index] = -1
+            self.diff_my_file[self.my_index] = -1
             return False
 
     def main_cmp(self):
-        deleted_lines = 0
         same = False
-        for my_line in self.my_file:
-            other_line = self.other_file[self.line]
+        while self.my_index < len(self.my_file):
+            my_line = self.my_file[self.my_index]
+            other_line = self.other_file[self.other_index]
             if my_line != other_line:
-                if same and not self.cmp_line(my_line.split(" "), other_line.split(" "), self.line):
-                    self.check_deleted_lines(my_line)
+                if same:
+                    if not self.check_other_deleted_lines(other_line):
+                        self.diff_other_lines.append(self.my_index)
+                    if not self.check_my_deleted_lines(my_line):
+                        self.diff_my_lines.append(self.other_index)
             else:
-                self.diff_other_file[self.line] = 1
-                self.diff_my_file[self.line] = 1
-                self.line += 1
+                self.diff_other_file[self.other_index] = -1
+                self.diff_my_file[self.my_index] = -1
+                self.my_index += 1
+                self.other_index += 1
                 same = True
+            print(self.other_index)
+        print(self.deleted_lines)
+        print(self.other_deleted_lines)
+        print(self.diff_my_lines)
+        print(self.diff_other_lines)
 
-    def check_deleted_lines(self, my_line):
+    def check_my_deleted_lines(self, my_line):
         deleted_lines = []
-        for line in self.other_file[self.line:]:
-            deleted_lines.append(self.line)
-            if self.cmp_line(my_line, line, self.line):
+        counter = self.other_index
+        self.other_index += 1
+        deleted_lines.append(self.other_index)
+        if my_line == "\n":
+            my_line = self.my_file[self.other_index]
+        for line in self.other_file[self.other_index:]:
+            if my_line == line:
+                self.other_index += 1
                 break
-            self.line += 1
+            self.other_index += 1
+            deleted_lines.append(self.other_index)
         else:
-            return
+            self.other_index = counter + 1
+            return False
         self.deleted_lines.append(deleted_lines)
+        return True
+
+    def check_other_deleted_lines(self, other_line):
+        deleted_lines = []
+        counter = self.my_index
+        self.my_index += 1
+        deleted_lines.append(self.my_index)
+        if other_line == "\n":
+            other_line = self.other_file[self.my_index]
+        for line in self.my_file[self.my_index:]:
+            if other_line == line:
+                self.my_index += 1
+                break
+            self.my_index += 1
+            deleted_lines.append(self.my_index)
+        else:
+            self.my_index = counter + 1
+            return True
+        self.other_deleted_lines.append(deleted_lines)
+        return False
 
 
 if __name__ == '__main__':
